472402e1c8248ee3a1d28f6c23b9d8e696a0dd3d
taiki-kuraishi <20122027co@gmail.com>
2023-10-21 09:51:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-import eel
-import io
-import sys
+import flet as ft
 
-eel.init('web')
 
-@eel.expose
-def receiveFile(file_name, file_contents):
-    with io.BytesIO(file_contents) as f:
-        contents = f.read().decode('utf-8')
-        print(contents)
+def main(page: ft.Page):
+    pass
 
-def onCloseWindow(page, sockets):
-    print(page + 'が閉じられました。プログラムを終了します。')
-    sys.exit()
 
-def main():
-    eel.init('web')
-    eel.start('index.html',mode='default', size=(700, 700), close_callback=onCloseWindow)
-
-if __name__ == '__main__':
-    main()
+ft.app(target=main)
