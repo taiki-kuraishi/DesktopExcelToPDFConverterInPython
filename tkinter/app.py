d21ce28894a20e3a820fa59adbdf517102be8dbf
taiki-kuraishi <20122027co@gmail.com>
2023-10-22 16:00:14 +0900

    add side frame and preview frame

--- tkinter/app.py
+++ tkinter/app.py
@@ -22,15 +22,14 @@ root.title(u"DesktopExcelToPDFConverterInPython")
 root.geometry("800x500")
 
 # frameの作成
-top_frame = ttk.Frame(root, bg='white')
-top_frame.pack(fill='x', padx=5, pady=5, ipadx=5, ipady=5)
+top_frame = ttk.Frame(root, bg='white', borderwidth=5, relief='ridge')
+top_frame.pack(fill='x')
 
 # columnconfigureを使用して、各列の幅を設定する
 top_frame.columnconfigure(0, weight=1)
 top_frame.columnconfigure(1, weight=1)
 top_frame.columnconfigure(2, weight=1)
 top_frame.columnconfigure(3, weight=1)
-top_frame.columnconfigure(4, weight=1)
 
 # radio bottom value
 radio_var = ttk.IntVar()
@@ -56,5 +55,30 @@ entry.focus_set()
 button = ttk.Button(top_frame, text="Click me!", command=select_path)
 button.configure(background='white', foreground='black')
 button.grid(row=1, column=3, sticky='w')
+
+# bttom frame
+bottom_frame = ttk.Frame(root, bg='Blue')
+bottom_frame.pack(fill='both', expand=True)
+
+bottom_frame.rowconfigure(0, weight=1)
+
+bottom_frame.columnconfigure(0, weight=1)
+bottom_frame.columnconfigure(1, weight=1)
+bottom_frame.columnconfigure(2, weight=1)
+bottom_frame.columnconfigure(3, weight=1)
+
+#side frame
+side_frame = ttk.Frame(bottom_frame, bg='white', borderwidth=5, relief='ridge')
+side_frame.grid(row=0, column=0, sticky='nswe')
+ttk.Label(side_frame, text='side frame').pack()
+ttk.Label(side_frame, text='side frame').pack()
+ttk.Label(side_frame, text='side frame').pack()
+ttk.Label(side_frame, text='side frame').pack()
+ttk.Label(side_frame, text='side frame').pack()
+
+#preview frame
+preview_frame = ttk.Frame(bottom_frame, bg='white', borderwidth=5, relief='ridge')
+preview_frame.grid(row=0, column=1, sticky='nswe',columnspan=3)
+
 
 root.mainloop()
