547a176531b4de124675a5f1c7bc36dfceec4f40
taiki-kuraishi <20122027co@gmail.com>
2023-10-23 23:29:39 +0900

    add file selector

--- pywebview/app.py
+++ pywebview/app.py
@@ -1,4 +1,23 @@
 import webview
 
-window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com')
-webview.start()
+
+class Api:
+    def add(self, a, b):
+        print("Python")
+        return a+b
+
+    def printPath(self, path):
+        print(path)
+
+    def choseFile(self):
+        global window
+        result = window.create_file_dialog(
+            webview.OPEN_DIALOG, allow_multiple=True)
+        print(result)
+        return True
+
+
+api = Api()
+window = webview.create_window(
+    "JS to Python", url="./web/index.html", js_api=api)
+webview.start(debug=True)
